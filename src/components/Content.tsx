b1f7bc6440d5923eaa093b62a7b3c3223a787aac
Guilherme Mattos <91619478+GuilCMattos@users.noreply.github.com>
2022-09-17 17:25:15 -0300

    add: useState works and Form & table dinamic

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,47 +1,60 @@
-import React from "react";
+
+import { useState } from "react";
 import Button from "../components/Button";
 import Form from "../components/Forms";
 import Layout from "../components/Layout";
 import Table from "../components/Table";
 import Client from "../core/Client";
 
-const Clients = [
-    new Client('Ana', 34, '1'),
-    new Client('Bia', 45, '2'),
-    new Client('Gui', 25, '3'),
-    new Client('Ju', 12, '4'),
-  ]
-  
-  function clientSelect(client: any) { 
-    console.log(client.name)
-  }
-  
-  function clientDelet(client: any) { 
-    console.log(client.name)
-  }
-
- 
-
-React.useState()
-
-
-
   export default function Content() { 
+
+    const Clients = [
+      new Client('Ana', 34, '1'),
+      new Client('Bia', 45, '2'),
+      new Client('Gui', 25, '3'),
+      new Client('Ju', 12, '4'),
+    ]
+    
+    function clientSelect(client: any) { 
+      console.log(client.name)
+    }
+    
+    function clientDelet(client: any) { 
+      console.log(client.name)
+    }
+
+    function saveClient(client: any) { 
+      console.log(client)
+    }
+
+    const [visible, setVisible] = useState<'table' | 'form' >('table')
+
+    
+
     return(
+      
         <Layout title="Cadastro Simples" >
-        
+
+          {visible === 'table' ? (
+
         <>
         <div className="flex justify-end">
-        <Button className="mb-4">Novo Cliente</Button>
+        <Button onClick={() => setVisible('form')} className="mb-4">Novo Cliente</Button>
         </div>
         <Table clients={Clients} clientsSelect={clientSelect} clientsDelet={clientDelet}></Table>
         
         </>
+
+           ) : (
+             
+             <Form 
+             client={Clients[1]}
+             clientChange={saveClient}
+             cancel={() => setVisible('table')}
+             />
+           ) } 
+        
           
-          <Form 
-          client={Clients[1]}
-          
-          />
       
       </Layout>
     )
--- src/components/Forms.tsx
+++ src/components/Forms.tsx
@@ -1,9 +1,11 @@
 import { useState } from "react";
 import Entry from "./Entry";
 import Button from './Button'
+import Client from "../core/Client";
 
 interface propsForm { 
     client: any
+    clientChange?: (client: any) => void
     cancel?: () => void
 
 }
@@ -24,7 +26,7 @@ export default function Form(props: propsForm) {
             <Entry text="Idade" value={age} onChange={setAge} type="number" />
 
             <div className="mt-7 flex justify-end">
-                <Button className="mr-2 from-blue-600 to-blue-800 ">
+                <Button className="mr-2 from-blue-600 to-blue-800 " onClick={() => props.clientChange?.(new Client(name, +age, id))}>
                {id ? 'Alterar' : 'Salvar'}
                 </Button>
                 <Button onClick={props.cancel} className=" from-gray-600 to-gray-800 "> Cancelar </Button>
